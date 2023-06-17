68d1a80281924799cf7e0a5df7c628a02bb931f1
MartinJDUni <demiliano148@gmail.com>
2023-06-17 13:24:45 -0600

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
+    
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
