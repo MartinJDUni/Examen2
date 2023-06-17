c9504e0ca1770ba033e7b72b55fdad9511e0ca31
CDanielSalas <carlos.salas.guido@est.una.ac.cr>
2023-06-17 16:31:27 -0600

    Update

--- src/pages/components/footer.tsx
+++ src/pages/components/footer.tsx
@@ -2,12 +2,14 @@ export default function Footer() {
     return (
         <footer className="text-gray-600 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-                <a>
-
-                </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
                     © 2023 Martin Jimenez, Daniel Salas
                 </p>
+
+                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+                    Siguenos en nuestras redes sociales como: @Cdanielguido y @Mextl
+                </p>
+
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a className="text-gray-500">
                         <svg
@@ -45,23 +47,6 @@ export default function Footer() {
                         >
                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                        </svg>
-                    </a>
-                    <a className="ml-3 text-gray-500">
-                        <svg
-                            fill="currentColor"
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="0"
-                            className="w-5 h-5"
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                stroke="none"
-                                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                            ></path>
-                            <circle cx="4" cy="4" r="2" stroke="none"></circle>
                         </svg>
                     </a>
                 </span>
