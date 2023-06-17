e77c42ae8ee394a54a4c5345a950be0a6baf531c
CDanielSalas <carlos.salas.guido@est.una.ac.cr>
2023-06-17 14:26:18 -0600

    Update footer

--- src/pages/components/footer.tsx
+++ src/pages/components/footer.tsx
@@ -2,6 +2,9 @@ export default function Footer() {
     return (
         <footer className="text-gray-600 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+                <a>
+
+                </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
                     © 2023 Martin Jimenez, Daniel Salas
                 </p>
